e849a0841c9ba04a42c02a1664441358c8ae3801
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-07-16 21:31:23 -0300

    add CLI

---
+++ crawlers/resolucao/src/cli.js
@@ -0,0 +1,7 @@
+const { crawler } = require("./crawler");
+
+(async () => {
+  const text = process.argv[2] || "cats;brazil";
+  const subreddits = text.split(";");
+  console.log(await crawler(subreddits));
+})();
